cd916f4b5494c5305da5d39be075af8e14d780d2
Luke Harmon <lukeharmon@Lukes-MacBook-Pro.local>
2016-10-10 10:03:34 -0700

    new ref style

--- chapters_preref/makeref_pandoc.sh
+++ chapters_preref/makeref_pandoc.sh
@@ -1,3 +1,3 @@
-pandoc -s --bibliography pcm_paperpile.bib --filter pandoc-citeproc --csl chicago-fullnote-bibliography.csl chapter1_introduction_pre.md -o t1.html
+pandoc -s --bibliography pcm_paperpile.bib --filter pandoc-citeproc --csl evolution.csl --metadata link-citations=true chapter1_introduction_pre.md -o t1.html
 cat metadata.yaml t1.html | sed s/%7B%7B%20/{{/ | sed s/%20%7D%7D/}}/ > ../chapter1_introduction.html
 rm t1.html
